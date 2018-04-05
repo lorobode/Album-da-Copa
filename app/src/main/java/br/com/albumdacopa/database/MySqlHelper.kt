561b6763a8e10ccefbadcaaa1281706a67d0fe00
Nichollas Fonseca <lorobode@gmail.com>
2018-04-05 19:52:59 -0300

    fix

--- app/src/main/java/br/com/albumdacopa/database/MySqlHelper.kt
+++ app/src/main/java/br/com/albumdacopa/database/MySqlHelper.kt
@@ -84,7 +84,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed stadium stickers
         for (i in 8..19) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (stadiumStickers?.get(i) ?: "stadium"),
+                    Sticker.COLUMN_NAME to (stadiumStickers?.get(i-8) ?: "stadium"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Stadium",
                     Sticker.COLUMN_QUANTITY to 0
@@ -93,7 +93,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed host cities stickers
         for (i in 20..31) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (hostCitiesStickers?.get(i) ?: "host city"),
+                    Sticker.COLUMN_NAME to (hostCitiesStickers?.get(i-20) ?: "host city"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Host City",
                     Sticker.COLUMN_QUANTITY to 0
@@ -102,7 +102,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed russia team stickers
         for (i in 32..51) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (russiaStickers?.get(i) ?: "russia"),
+                    Sticker.COLUMN_NAME to (russiaStickers?.get(i-32) ?: "russia"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Russia",
                     Sticker.COLUMN_QUANTITY to 0
@@ -111,7 +111,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed saudi arabia team stickers
         for (i in 52..71) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (saudiArabiaStickers?.get(i) ?: "saudi arabia"),
+                    Sticker.COLUMN_NAME to (saudiArabiaStickers?.get(i-52) ?: "saudi arabia"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Saudi Arabia",
                     Sticker.COLUMN_QUANTITY to 0
@@ -120,7 +120,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Egypt Team stickers
         for (i in 72..91) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (egyptStickers?.get(i) ?: "egypt"),
+                    Sticker.COLUMN_NAME to (egyptStickers?.get(i-72) ?: "egypt"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Egypt Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -129,7 +129,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Uruguay Team stickers
         for (i in 92..111) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (uruguayStickers?.get(i) ?: "uruguay"),
+                    Sticker.COLUMN_NAME to (uruguayStickers?.get(i-92) ?: "uruguay"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Uruguay Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -138,7 +138,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Portugal Team stickers
         for (i in 112..131) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (portugalStickers?.get(i) ?: "portugal"),
+                    Sticker.COLUMN_NAME to (portugalStickers?.get(i-112) ?: "portugal"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Portugal Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -147,7 +147,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Spain Team stickers
         for (i in 132..151) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (spainStickers?.get(i) ?: "spain"),
+                    Sticker.COLUMN_NAME to (spainStickers?.get(i-132) ?: "spain"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Spain Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -156,7 +156,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Morocco Team stickers
         for (i in 152..171) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (moroccoStickers?.get(i) ?: "morocco"),
+                    Sticker.COLUMN_NAME to (moroccoStickers?.get(i-152) ?: "morocco"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Morocco Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -165,7 +165,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Iran Team stickers
         for (i in 172..191) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (iranStickers?.get(i) ?: "iran"),
+                    Sticker.COLUMN_NAME to (iranStickers?.get(i-172) ?: "iran"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Iran Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -174,7 +174,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed France Team stickers
         for (i in 192..211) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (franceStickers?.get(i) ?: "france"),
+                    Sticker.COLUMN_NAME to (franceStickers?.get(i-192) ?: "france"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "France Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -183,7 +183,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Australia Team stickers
         for (i in 212..231) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (australiaStickers?.get(i) ?: "australia"),
+                    Sticker.COLUMN_NAME to (australiaStickers?.get(i-212) ?: "australia"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Australia Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -192,7 +192,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Peru Team stickers
         for (i in 232..251) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (peruStickers?.get(i) ?: "peru"),
+                    Sticker.COLUMN_NAME to (peruStickers?.get(i-232) ?: "peru"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Peru Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -201,7 +201,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Denmark Team stickers
         for (i in 252..271) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (denmarkStickers?.get(i) ?: "denmark"),
+                    Sticker.COLUMN_NAME to (denmarkStickers?.get(i-252) ?: "denmark"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Denmark Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -210,7 +210,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Argentina Team stickers
         for (i in 272..291) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (argentinaStickers?.get(i) ?: "argentina"),
+                    Sticker.COLUMN_NAME to (argentinaStickers?.get(i-272) ?: "argentina"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Argentina Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -219,7 +219,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Iceland Team stickers
         for (i in 292..311) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (icelandStickers?.get(i) ?: "iceland"),
+                    Sticker.COLUMN_NAME to (icelandStickers?.get(i-292) ?: "iceland"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Iceland Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -228,7 +228,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Croatia Team stickers
         for (i in 312..331) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (croatiaStickers?.get(i) ?: "croatia"),
+                    Sticker.COLUMN_NAME to (croatiaStickers?.get(i-312) ?: "croatia"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Croatia Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -237,7 +237,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Nigeria Team stickers
         for (i in 332..351) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (nigeriaStickers?.get(i) ?: "nigeria"),
+                    Sticker.COLUMN_NAME to (nigeriaStickers?.get(i-332) ?: "nigeria"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Nigeria Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -246,7 +246,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Brazil Team stickers
         for (i in 352..371) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (brazilStickers?.get(i) ?: "brazil"),
+                    Sticker.COLUMN_NAME to (brazilStickers?.get(i-352) ?: "brazil"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Brazil Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -255,7 +255,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Switzerland Team stickers
         for (i in 372..391) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (switzerlandStickers?.get(i) ?: "switzerland"),
+                    Sticker.COLUMN_NAME to (switzerlandStickers?.get(i-372) ?: "switzerland"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Switzerland Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -264,7 +264,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Costa Rica Team stickers
         for (i in 392..411) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (costaRicaStickers?.get(i) ?: "costa rica"),
+                    Sticker.COLUMN_NAME to (costaRicaStickers?.get(i-392) ?: "costa rica"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Costa Rica Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -273,7 +273,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Serbia Team stickers
         for (i in 412..431) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (serbiaStickers?.get(i) ?: "serbia"),
+                    Sticker.COLUMN_NAME to (serbiaStickers?.get(i-412) ?: "serbia"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Serbia Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -282,7 +282,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Germany Team stickers
         for (i in 432..451) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (germanyStickers?.get(i) ?: "germany"),
+                    Sticker.COLUMN_NAME to (germanyStickers?.get(i-432) ?: "germany"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Germany Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -291,7 +291,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Mexico Team stickers
         for (i in 452..471) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (mexicoStickers?.get(i) ?: "mexico"),
+                    Sticker.COLUMN_NAME to (mexicoStickers?.get(i-452) ?: "mexico"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Mexico Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -300,7 +300,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Sweden Team stickers
         for (i in 472..491) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (swedenStickers?.get(i) ?: "sweden"),
+                    Sticker.COLUMN_NAME to (swedenStickers?.get(i-472) ?: "sweden"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Sweden Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -309,7 +309,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Korea Republic Team stickers
         for (i in 492..511) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (koreaRepublicStickers?.get(i) ?: "korea republic"),
+                    Sticker.COLUMN_NAME to (koreaRepublicStickers?.get(i-492) ?: "korea republic"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Korea Republic Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -318,7 +318,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Belgium Team stickers
         for (i in 512..531) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (belgiumStickers?.get(i) ?: "belgium"),
+                    Sticker.COLUMN_NAME to (belgiumStickers?.get(i-512) ?: "belgium"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Belgium Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -327,7 +327,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Panama Team stickers
         for (i in 532..551) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (panamaStickers?.get(i) ?: "panama"),
+                    Sticker.COLUMN_NAME to (panamaStickers?.get(i-532) ?: "panama"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Panama Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -336,7 +336,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Tunisia Team stickers
         for (i in 552..571) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (tunisiaStickers?.get(i) ?: "tunisia"),
+                    Sticker.COLUMN_NAME to (tunisiaStickers?.get(i-552) ?: "tunisia"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Tunisia Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -345,7 +345,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed England Team stickers
         for (i in 572..591) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (englandStickers?.get(i) ?: "england"),
+                    Sticker.COLUMN_NAME to (englandStickers?.get(i-572) ?: "england"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "England Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -354,7 +354,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Poland Team stickers
         for (i in 592..611) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (polandStickers?.get(i) ?: "poland"),
+                    Sticker.COLUMN_NAME to (polandStickers?.get(i-592) ?: "poland"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Poland Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -363,7 +363,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Senegal Team stickers
         for (i in 612..631) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (senegalStickers?.get(i) ?: "senegal"),
+                    Sticker.COLUMN_NAME to (senegalStickers?.get(i-612) ?: "senegal"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Senegal Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -372,7 +372,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Colombia Team stickers
         for (i in 632..651) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (colombiaStickers?.get(i) ?: "colombia"),
+                    Sticker.COLUMN_NAME to (colombiaStickers?.get(i-632) ?: "colombia"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Colombia Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -381,7 +381,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Japan Team stickers
         for (i in 652..671) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (japanStickers?.get(i) ?: "japan"),
+                    Sticker.COLUMN_NAME to (japanStickers?.get(i-652) ?: "japan"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Japan Team",
                     Sticker.COLUMN_QUANTITY to 0
@@ -390,7 +390,7 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
         //Seed Legends stickers
         for (i in 672..681) {
             db.insert(Sticker.TABLE_NAME,
-                    Sticker.COLUMN_NAME to (legendsStickers?.get(i) ?: "legends"),
+                    Sticker.COLUMN_NAME to (legendsStickers?.get(i-672) ?: "legends"),
                     Sticker.COLUMN_NUMBER to i,
                     Sticker.COLUMN_TYPE to "Legends",
                     Sticker.COLUMN_QUANTITY to 0
