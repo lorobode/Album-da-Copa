53f57c1f3c66eba6c28ddf9f01218ab131d9279f
Nichollas Fonseca <lorobode@gmail.com>
2018-04-05 19:38:51 -0300

    Seed database and add quantity

--- app/src/main/java/br/com/albumdacopa/database/MySqlHelper.kt
+++ app/src/main/java/br/com/albumdacopa/database/MySqlHelper.kt
@@ -31,15 +31,369 @@ class MySqlHelper(ctx: Context) : ManagedSQLiteOpenHelper(ctx, "albumdb") {
                 "_id" to INTEGER + PRIMARY_KEY + AUTOINCREMENT,
                 Sticker.COLUMN_NAME to TEXT,
                 Sticker.COLUMN_NUMBER to INTEGER,
-                Sticker.COLUMN_TYPE to TEXT)
+                Sticker.COLUMN_TYPE to TEXT,
+                Sticker.COLUMN_QUANTITY to INTEGER
+                )
 
         val customStickers = res?.getStringArray(R.array.custom_stickers)
+        val stadiumStickers = res?.getStringArray(R.array.stadium_stickers)
+        val hostCitiesStickers = res?.getStringArray(R.array.host_cities_stickers)
+        val russiaStickers = res?.getStringArray(R.array.russia_stickers)
+        val saudiArabiaStickers = res?.getStringArray(R.array.saudi_arabia_stickers)
+        val egyptStickers = res?.getStringArray(R.array.egypt_stickers)
+        val uruguayStickers = res?.getStringArray(R.array.uruguay_stickers)
+        val portugalStickers = res?.getStringArray(R.array.portugal_stickers)
+        val spainStickers = res?.getStringArray(R.array.spain_stickers)
+        val moroccoStickers = res?.getStringArray(R.array.morocco_stickers)
+        val iranStickers = res?.getStringArray(R.array.iran_stickers)
+        val franceStickers = res?.getStringArray(R.array.france_stickers)
+        val australiaStickers = res?.getStringArray(R.array.australia_stickers)
+        val peruStickers = res?.getStringArray(R.array.peru_stickers)
+        val denmarkStickers = res?.getStringArray(R.array.denmark_stickers)
+        val argentinaStickers = res?.getStringArray(R.array.argentina_stickers)
+        val icelandStickers = res?.getStringArray(R.array.iceland_stickers)
+        val croatiaStickers = res?.getStringArray(R.array.croatia_stickers)
+        val nigeriaStickers = res?.getStringArray(R.array.nigeria_stickers)
+        val brazilStickers = res?.getStringArray(R.array.brazil_stickers)
+        val switzerlandStickers = res?.getStringArray(R.array.switzerland_stickers)
+        val costaRicaStickers = res?.getStringArray(R.array.costa_rica_stickers)
+        val serbiaStickers = res?.getStringArray(R.array.serbia_stickers)
+        val germanyStickers = res?.getStringArray(R.array.germany_stickers)
+        val mexicoStickers = res?.getStringArray(R.array.mexico_stickers)
+        val swedenStickers = res?.getStringArray(R.array.sweden_stickers)
+        val koreaRepublicStickers = res?.getStringArray(R.array.korea_republic_stickers)
+        val belgiumStickers = res?.getStringArray(R.array.belgium_stickers)
+        val panamaStickers = res?.getStringArray(R.array.panama_stickers)
+        val tunisiaStickers = res?.getStringArray(R.array.tunisia_stickers)
+        val englandStickers = res?.getStringArray(R.array.england_stickers)
+        val polandStickers = res?.getStringArray(R.array.poland_stickers)
+        val senegalStickers = res?.getStringArray(R.array.senegal_stickers)
+        val colombiaStickers = res?.getStringArray(R.array.colombia_stickers)
+        val japanStickers = res?.getStringArray(R.array.japan_stickers)
+        val legendsStickers = res?.getStringArray(R.array.legends_stickers)
 
+        //Seed custom stickers
         for (i in 0..7) {
             db.insert(Sticker.TABLE_NAME,
                     Sticker.COLUMN_NAME to (customStickers?.get(i) ?: "custom"),
                     Sticker.COLUMN_NUMBER to i,
-                    Sticker.COLUMN_TYPE to "custom"
+                    Sticker.COLUMN_TYPE to "Custom",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed stadium stickers
+        for (i in 8..19) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (stadiumStickers?.get(i) ?: "stadium"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Stadium",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed host cities stickers
+        for (i in 20..31) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (hostCitiesStickers?.get(i) ?: "host city"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Host City",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed russia team stickers
+        for (i in 32..51) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (russiaStickers?.get(i) ?: "russia"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Russia",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed saudi arabia team stickers
+        for (i in 52..71) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (saudiArabiaStickers?.get(i) ?: "saudi arabia"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Saudi Arabia",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Egypt Team stickers
+        for (i in 72..91) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (egyptStickers?.get(i) ?: "egypt"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Egypt Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Uruguay Team stickers
+        for (i in 92..111) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (uruguayStickers?.get(i) ?: "uruguay"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Uruguay Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Portugal Team stickers
+        for (i in 112..131) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (portugalStickers?.get(i) ?: "portugal"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Portugal Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Spain Team stickers
+        for (i in 132..151) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (spainStickers?.get(i) ?: "spain"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Spain Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Morocco Team stickers
+        for (i in 152..171) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (moroccoStickers?.get(i) ?: "morocco"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Morocco Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Iran Team stickers
+        for (i in 172..191) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (iranStickers?.get(i) ?: "iran"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Iran Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed France Team stickers
+        for (i in 192..211) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (franceStickers?.get(i) ?: "france"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "France Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Australia Team stickers
+        for (i in 212..231) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (australiaStickers?.get(i) ?: "australia"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Australia Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Peru Team stickers
+        for (i in 232..251) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (peruStickers?.get(i) ?: "peru"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Peru Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Denmark Team stickers
+        for (i in 252..271) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (denmarkStickers?.get(i) ?: "denmark"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Denmark Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Argentina Team stickers
+        for (i in 272..291) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (argentinaStickers?.get(i) ?: "argentina"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Argentina Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Iceland Team stickers
+        for (i in 292..311) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (icelandStickers?.get(i) ?: "iceland"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Iceland Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Croatia Team stickers
+        for (i in 312..331) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (croatiaStickers?.get(i) ?: "croatia"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Croatia Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Nigeria Team stickers
+        for (i in 332..351) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (nigeriaStickers?.get(i) ?: "nigeria"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Nigeria Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Brazil Team stickers
+        for (i in 352..371) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (brazilStickers?.get(i) ?: "brazil"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Brazil Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Switzerland Team stickers
+        for (i in 372..391) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (switzerlandStickers?.get(i) ?: "switzerland"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Switzerland Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Costa Rica Team stickers
+        for (i in 392..411) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (costaRicaStickers?.get(i) ?: "costa rica"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Costa Rica Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Serbia Team stickers
+        for (i in 412..431) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (serbiaStickers?.get(i) ?: "serbia"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Serbia Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Germany Team stickers
+        for (i in 432..451) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (germanyStickers?.get(i) ?: "germany"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Germany Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Mexico Team stickers
+        for (i in 452..471) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (mexicoStickers?.get(i) ?: "mexico"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Mexico Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Sweden Team stickers
+        for (i in 472..491) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (swedenStickers?.get(i) ?: "sweden"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Sweden Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Korea Republic Team stickers
+        for (i in 492..511) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (koreaRepublicStickers?.get(i) ?: "korea republic"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Korea Republic Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Belgium Team stickers
+        for (i in 512..531) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (belgiumStickers?.get(i) ?: "belgium"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Belgium Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Panama Team stickers
+        for (i in 532..551) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (panamaStickers?.get(i) ?: "panama"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Panama Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Tunisia Team stickers
+        for (i in 552..571) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (tunisiaStickers?.get(i) ?: "tunisia"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Tunisia Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed England Team stickers
+        for (i in 572..591) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (englandStickers?.get(i) ?: "england"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "England Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Poland Team stickers
+        for (i in 592..611) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (polandStickers?.get(i) ?: "poland"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Poland Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Senegal Team stickers
+        for (i in 612..631) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (senegalStickers?.get(i) ?: "senegal"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Senegal Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Colombia Team stickers
+        for (i in 632..651) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (colombiaStickers?.get(i) ?: "colombia"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Colombia Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Japan Team stickers
+        for (i in 652..671) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (japanStickers?.get(i) ?: "japan"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Japan Team",
+                    Sticker.COLUMN_QUANTITY to 0
+            )
+        }
+        //Seed Legends stickers
+        for (i in 672..681) {
+            db.insert(Sticker.TABLE_NAME,
+                    Sticker.COLUMN_NAME to (legendsStickers?.get(i) ?: "legends"),
+                    Sticker.COLUMN_NUMBER to i,
+                    Sticker.COLUMN_TYPE to "Legends",
+                    Sticker.COLUMN_QUANTITY to 0
             )
         }
     }
--- app/src/main/java/br/com/albumdacopa/model/Sticker.kt
+++ app/src/main/java/br/com/albumdacopa/model/Sticker.kt
@@ -3,11 +3,12 @@ package br.com.albumdacopa.model
 /**
  * Created by Nichollas on 05/04/2018.
  */
-data class Sticker(val id: Int, val name: String, val number: Int, val type: String) {
+data class Sticker(val id: Int, val name: String, val number: Int, val type: String, val quantity: Int) {
     companion object {
         val TABLE_NAME = "Sticker"
         val COLUMN_NAME = "name"
         val COLUMN_NUMBER = "number"
         val COLUMN_TYPE = "type"
+        val COLUMN_QUANTITY = "quantity"
     }
 }
